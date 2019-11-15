e37636cc15740aa167a969cc22e66c0d1908d13b
Terje Sandstrom <terje.sandstrom.personal@outlook.com>
2019-11-15 22:37:38 +0100

    code

---
+++ UnitTest1.cs
@@ -0,0 +1,15 @@
+using NUnit.Framework;
+
+namespace example1
+{
+    public class Tests
+    {
+        
+
+        [Test]
+        public void Test1()
+        {
+            Assert.That(true);
+        }
+    }
+}
